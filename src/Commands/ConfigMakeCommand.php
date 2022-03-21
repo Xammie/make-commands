03fbbd63d09fc03540701bebbcb13522858dfede
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-03-21 21:17:53 +0100

    Fixed incorrect config filepath

--- src/Commands/ConfigMakeCommand.php
+++ src/Commands/ConfigMakeCommand.php
@@ -3,6 +3,7 @@
 namespace Xammie\MakeCommands\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
 
 class ConfigMakeCommand extends GeneratorCommand
 {
@@ -17,5 +18,17 @@ class ConfigMakeCommand extends GeneratorCommand
     protected function stubName(): string
     {
         return 'config';
+    }
+
+    protected function getPath($name)
+    {
+        $name = Str::replaceFirst($this->rootNamespace(), '', $name);
+
+        return str_replace('\\', '/', base_path($name)) . '.php';
+    }
+
+    protected function alreadyExists($rawName): bool
+    {
+        return $this->files->exists($this->getPath($this->qualifyClass($rawName)));
     }
 }
